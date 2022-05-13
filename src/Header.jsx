af8bfe9125bcfacdbdbcfc2d4a2254cc32898ae3
pepelina <pepelina@hotmail.com>
2022-05-13 12:56:19 +0200

    bara ändrat knappnamn från bakelse till bakelser

--- src/Header.jsx
+++ src/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => (
         </div>
         <div>sök fält Ingrida</div>
         <div  className={styles.categoriButton}>
-          <button className={styles.btn1}><NavLink to="/" className={({isActive}) => isActive ? styles.activeLink : styles.inactiveLink} style={Btn1Style}>BAKELSE</NavLink></button>
+          <button className={styles.btn1}><NavLink to="/" className={({isActive}) => isActive ? styles.activeLink : styles.inactiveLink} style={Btn1Style}>BAKELSER</NavLink></button>
           <button className={styles.btn2}><NavLink to="/Kategorisida" className={({isActive}) => isActive ? styles.activeLink : styles.inactiveLink} style={Btn1Style}>KAKOR</NavLink></button>
           <button className={styles.btn3}><NavLink to="/Receptsida" className={({isActive}) => isActive ? styles.activeLink : styles.inactiveLink} style={Btn3Style}>TÅRTOR</NavLink></button>
         </div>
