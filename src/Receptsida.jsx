26b6f409522030a120a1b43b1f7bc426e71c1963
pepelina <pepelina@hotmail.com>
2022-05-16 11:15:43 +0200

    Liveserverport

--- src/Receptsida.jsx
+++ src/Receptsida.jsx
@@ -5,7 +5,7 @@ const Receptsida = () => (
         <h2>Receptsida</h2>
         <p>Vi är ett härligt gäng </p>
         <h3>Kommentarer</h3>
-        <Form isSent={false}/>
+        <Form isSent={true}/>
     </>
 )
 
