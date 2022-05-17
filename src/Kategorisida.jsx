c6034df4ea91464f8eed96b21194ec9bc79c92bc
Ingrid-WE <kutriene.ingrida@gmail.com>
2022-05-17 14:41:51 +0200

    recept sidan fungerar inte

--- src/Kategorisida.jsx
+++ src/Kategorisida.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import styles from "../App.module.css";
+import styles from "./App.module.css";
 import { NavLink } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 const imgStyle = {
     width: "100%",
@@ -12,19 +13,21 @@ const receptLink ={
     color: "#8B6E4E"
 }
 const Bakelser = () => {
+    const {categories}  = useParams();
     const [IsRecept, setRecept] = useState();
 useEffect(() => {   
-        fetch("https://paprika-bxu3y.ondigitalocean.app/categories/Bakelser/recipes")
-        .then(res => res.json())
-        .then(data => setRecept(data))
+     fetch(`https://paprika-bxu3y.ondigitalocean.app/categories/${categories}/recipes`)
+         .then(res => res.json())
+         .then(data => setRecept(data))
     
-}, [])
+}, [categories])
 
-    //console.log(IsRecept);
+   console.log(categories);
 
     const Recepten = IsRecept?.map((recept) =>
         
-        <div ><NavLink to="/Receptsida" style={receptLink}>
+    
+        <div ><NavLink to={`/recipes/${recept._id}`} style={receptLink}>
             <img alt={recept.title} src={recept.imageUrl } style={imgStyle} />
             <div className={styles.cardText}>
                 <h2><b>{recept.title}</b> {recept.avgRating}</h2>
--- src/Receptsida.jsx
+++ src/Receptsida.jsx
@@ -1,6 +1,7 @@
 import Form from './Form';
 import { useState } from "react";
 import { useEffect } from "react";
+import { useParams } from 'react-router-dom';
 import styles from "./App.module.css";
 const imgStyle = {
     width: "100%",
@@ -9,38 +10,39 @@ const imgStyle = {
 }
 
 const Receptsida = () => {
+    const {recipesId} = useParams();
     const [IsRecept, setRecept] = useState();
 useEffect(() => {   
-        fetch("https://paprika-bxu3y.ondigitalocean.app/recipes?query=id")
-        .then(res => res.json())
-        .then(data => setRecept(data))
-    
-}, [])
+     fetch(`https://paprika-bxu3y.ondigitalocean.app/recipes/${recipesId}`)
+         .then(res => res.json())
+         .then(data => setRecept(data))
+  
+}, [recipesId])
 
-    console.log(IsRecept);
 
-    const Recepten = IsRecept?.map((recept) =>
-        
-        <div key={recept.id}>
-            <img alt={recept.title} src={recept.imageUrl } style={imgStyle} />
-            <div className={styles.receptCardText}>
-                <h2><b>{recept.title}</b> {recept.avgRating}</h2>
-                <p> {recept.description}</p>
-                <p><ol>
-                    {recept.instructions.map(instruction =>
-                    <li> {instruction}</li>)}
-               </ol> </p>
-                <b>{recept.categories} || {recept.timeInMins} Minuter</b>
-            </div>
-        </div>
+console.log(IsRecept)
+
+
+ /* 
        
-    )
+        */
 
     return (
   
         <>
             <div className={styles.receptFlexContainer}>
-               {Recepten}
+            <div key={IsRecept.id}>
+            <img alt={IsRecept.title} src={IsRecept.imageUrl } style={imgStyle} />
+            <div className={styles.receptCardText}>
+                <h2><b>{IsRecept.title}</b> {IsRecept.avgRating}</h2>
+                <p> {IsRecept.description}</p>
+                <p><ol>
+                    {IsRecept.instructions.map(instruction =>
+                    <li> {instruction}</li>)}
+               </ol> </p>
+                <b>{IsRecept.categories} || {IsRecept.timeInMins} Minuter</b>
+            </div>
+        </div>
             </div>
             <div className={styles.form}>
                 <h3>Kommentarer</h3>
