48f502b4af563f26cb8dd9e888a32307ed3a70ca
DanaAlhawari <dana.alhawari@iths.se>
2022-05-20 12:02:47 +0200

    changa in form

--- src/Form.jsx
+++ src/Form.jsx
@@ -6,20 +6,28 @@ import styles from "./App.module.css";
 const Form = () => {
      const [name, setName] = useState('');
     const [comment, setComment] = useState('');
-    const isEnabled = name.length > 0 && comment.length > 0;
+    const isEnabled = () => {
+        if (name.length > 0 && comment.length > 0) {
+            const date = new Date().toDateString();
+            console.log(name, comment, date); 
+            return console.log('Tack för din kommentarer');  
+        } else {    
+            return console.log('fyll i alla fält');
+        }   
+    }
+    
     const onSubmit = (e) => {
         e.preventDefault();
         setName('');
         setComment('');
-        const date = new Date().toDateString();
-        console.log(name, comment, date);
+     
      
     }
    
 
     return ( 
         
-        <form onSubmit = {onSubmit}  disabled={!isEnabled}>
+        <form onSubmit = {onSubmit}  >
             <input type="text" placeholder="Namn" value={name}
                 onChange={(e) => {
                     setName(e.target.value);
@@ -29,7 +37,7 @@ const Form = () => {
                 onChange={(e) => {
                     setComment(e.target.value);
                 }}  ></textarea>
-            <button className={styles.submit}>Skicka</button>
+            <button className={styles.submit} onClick={() =>isEnabled()}>Skicka</button>
             
         </form>
     )
