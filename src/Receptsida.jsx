99bcc1fb9c65b12f4da4cb24c916af61319553cf
DanaAlhawari <dana.alhawari@iths.se>
2022-06-07 13:28:10 +0200

    add css

--- src/Receptsida.jsx
+++ src/Receptsida.jsx
@@ -39,25 +39,33 @@ const Receptsida = () => {
             <Row>{Recept ?
                 <Col key={Recept._id} >
                     <Card className={styles.receptFlexContainer}>
-                        <Card.Img alt={Recept.title} src={Recept.imageUrl} style={imgStyle} />
-                        <Card.Body className={styles.receptCardText}>
-                            <h2>{Recept.title}</h2>
-                            <Card.Text>
-                                {Recept.description}
-                                <ol>
-                                    {Recept.instructions.map(instruction =>
-                                        <li > {instruction}</li>)}
-                                </ol>
-                                <strong>{Recept.categories} || {Recept.timeInMins} Minuter </strong>
-                                <ReactStars
-                                    count={5}
-                                    edit={false}
-                                    size={35}
-                                    isHalf={true}
-                                    color={'#EAEEC5'}
-                                    activeColor={"#8B6E4E"}
-                                    value={Recept.avgRating} />
-                            </Card.Text>
+                        {/* <Card.Img alt={Recept.title} src={Recept.imageUrl} style={imgStyle} /> */}
+                        <Card.Body className={styles.receptCardText} style={{ padding: 0 }} >
+                            <div style={{
+                                backgroundImage: `url(${Recept.imageUrl}) `, backgroundRepeat: 'no-repeat',
+                                backgroundSize: 'cover', backgroundPosition: 'center'
+                            }} className={styles.cardBody}>
+                                <div style={{ backgroundColor: '#FFF8F0', opacity: '0.7', padding: 16 }}>
+                                    <h2>{Recept.title}</h2>
+
+                                    <Card.Text>
+                                        {Recept.description}
+                                        <ol>
+                                            {Recept.instructions.map(instruction =>
+                                                <li > {instruction}</li>)}
+                                        </ol>
+                                        <strong>{Recept.categories} || {Recept.timeInMins} Minuter </strong>
+                                        <ReactStars
+                                            count={5}
+                                            edit={false}
+                                            size={35}
+                                            isHalf={true}
+                                            color={'#EAEEC5'}
+                                            activeColor={"#8B6E4E"}
+                                            value={Recept.avgRating} />
+                                    </Card.Text>
+                                </div>
+                            </div>
                         </Card.Body>
                     </Card>
                 </Col>
